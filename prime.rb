b96b554b521fa8acb019692e0dc9b15db66a7010
Katleiah <katleiahramos@gmail.com>
2018-03-12 16:38:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Add  code here!
 def prime?(number)
   if number <= 1
@@ -13,9 +14,10 @@ def prime?(number)
       if number % i == 0 || number % ( i + 2 ) == 0
         return false
       end
-      i += 6
+    i += 6
     return true
     end
+
    return true
   end
 end
