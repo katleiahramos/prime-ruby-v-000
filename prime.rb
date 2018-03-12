e20a95d9128325c3d342cbbb5a93c2afb0c690da
Katleiah <katleiahramos@gmail.com>
2018-03-12 16:24:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  elsif number % 2 == 0 || number % 3 == 0
+    return false
+  else
+    i = 5
+    w = 2 
+    while i * i <= number
+      if number % i == 0 
+        return false
+      end
+      i += w 
+      w = 6 - w
+    end
+    return true
+  end
+end
